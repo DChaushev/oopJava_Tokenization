15a4a683f828fd36b7ec4ebc2fc14332906b9866
Dimitar Chaushev <d.chaushev073@gmail.com>
2015-02-05 21:35:07 +0200

    adding initial version of the token generator

---
+++ src/com/fmi/oopjava/tokenGenerator/TokenGenerator.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fmi.oopjava.tokenGenerator;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Dimitar
+ */
+public class TokenGenerator {
+
+    private static final Random random = new Random();
+
+    public static String generateToken(String cardNumber) {
+
+        int digit;
+        int firstDigitPos; // will be used if the sum % 10 == 0. Then we'll set 
+                           // as first digit the next one from the allowedFirstDigits.
+        int[] allowedFirstDigits = {1, 2, 7, 8, 9};
+        int[] result = new int[cardNumber.length()];
+        int sum = 0;
+
+        firstDigitPos = random.nextInt(allowedFirstDigits.length);
+        // set the first digit
+        result[0] = allowedFirstDigits[firstDigitPos];
+        sum += allowedFirstDigits[firstDigitPos];
+
+        // set the next n-4 digits
+        for (int i = 1; i < cardNumber.length() - 4; i++) {
+
+            do {
+                digit = random.nextInt(10);
+            } while (digit == cardNumber.charAt(i) - (int) '0');
+            result[i] = digit;
+            sum += digit;
+        }
+        // set the last 4 digits (they must be the same as the card's
+        for (int i = cardNumber.length() - 4; i < cardNumber.length(); i++) {
+            result[i] = cardNumber.charAt(i) - (int) '0';
+            sum += cardNumber.charAt(i) - (int) '0';
+        }
+        // if the sum is divisable by 10, we change the first digit
+        if (sum % 10 == 0) {
+            result[0] = allowedFirstDigits[(firstDigitPos + 1) % allowedFirstDigits.length];
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < result.length; i++) {
+            res.append(result[i]);
+        }
+        
+        return res.toString();
+    }
+}
---
+++ test/com/fmi/oopjava/tokenizer/tests/TokenGeneratorTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fmi.oopjava.tokenizer.tests;
+
+import com.fmi.oopjava.tokenGenerator.TokenGenerator;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dimitar
+ */
+public class TokenGeneratorTest {
+
+    private static String cardNumber1;
+    private static String cardNumber2;
+    private static String cardNumber3;
+    private static char[] allowedFirstDigits;
+
+    @BeforeClass
+    public static void setUpClass() {
+        cardNumber1 = "4563960122019991";
+        cardNumber2 = "1234567891011123";
+        cardNumber3 = "1111222333449992";
+        allowedFirstDigits = new char[]{'1', '2', '7', '8', '9'};
+    }
+
+    @Test
+    public void testTokenGenerator(){
+        assertTrue(checkToken(cardNumber1, TokenGenerator.generateToken(cardNumber1)));
+        assertTrue(checkToken(cardNumber2, TokenGenerator.generateToken(cardNumber2)));
+        assertTrue(checkToken(cardNumber3, TokenGenerator.generateToken(cardNumber3)));
+    }
+    
+    private boolean checkToken(String cardNumber, String token) {
+        if (cardNumber.length() != token.length()) {
+            return false;
+        }
+
+        boolean check = false;
+        for (int i = 0; i < allowedFirstDigits.length; i++) {
+            if (token.charAt(0) == allowedFirstDigits[i]) {
+                check = true;
+            }
+        }
+        if (!check) {
+            return false;
+        }
+
+        for (int i = cardNumber.length() - 4; i < cardNumber.length(); i++) {
+            if (cardNumber.charAt(i) != token.charAt(i)) {
+                return false;
+            }
+        }
+
+        for (int i = 1; i < cardNumber.length() - 4; i++) {
+            if (cardNumber.charAt(i) == token.charAt(i)) {
+                return false;
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < token.length(); i++) {
+            sum += token.charAt(i) - (int) '0';
+        }
+        if (sum % 10 == 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
